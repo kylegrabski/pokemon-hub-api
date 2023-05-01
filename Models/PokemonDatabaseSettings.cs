37933bcc11757c78f7977bbefd87f6abe84c1570
kylegrabski <kyle.grabski@gmail.com>
2023-04-30 19:40:32 -0700

    add mongodb connection class

---
+++ Models/PokemonDatabaseSettings.cs
@@ -0,0 +1,10 @@
+namespace PokemonApi.Models;
+
+public class PokemonDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string CollectionName { get; set; } = null!;
+}
