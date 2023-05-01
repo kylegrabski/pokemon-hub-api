dc8b946c874c5a146beac794fc08e72eeb4ae87f
kylegrabski <kyle.grabski@gmail.com>
2023-04-30 20:27:27 -0700

    mapped controllers

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using PokemonApi.Models;
+using PokemonApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<PokemonDatabaseSettings>(
     builder.Configuration.GetSection("PokemonDatabase"));
+
+// Create Singleton class for Pokemon/MongoDB service
+builder.Services.AddSingleton<PokemonService>();
+
 
 builder.Services.AddCors(options =>
 {
@@ -16,71 +21,12 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-
 app.UseCors();
 
-
-app.MapGet("/", () =>  {
-    Console.WriteLine("WOWZA");
-    return "Hello there";
-});
+app.MapControllers();
 
 app.Run();
-
-// using Microsoft.EntityFrameworkCore;
-
-// var builder = WebApplication.CreateBuilder(args);
-// builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
-// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-// var app = builder.Build();
-
-// app.MapGet("/todoitems", async (TodoDb db) =>
-//     await db.Todos.ToListAsync());
-
-// app.MapGet("/todoitems/complete", async (TodoDb db) =>
-//     await db.Todos.Where(t => t.IsComplete).ToListAsync());
-
-// app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-//     await db.Todos.FindAsync(id)
-//         is Todo todo
-//             ? Results.Ok(todo)
-//             : Results.NotFound());
-
-// app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
-// {
-//     db.Todos.Add(todo);
-//     await db.SaveChangesAsync();
-
-//     return Results.Created($"/todoitems/{todo.Id}", todo);
-// });
-
-// app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
-// {
-//     var todo = await db.Todos.FindAsync(id);
-
-//     if (todo is null) return Results.NotFound();
-
-//     todo.Name = inputTodo.Name;
-//     todo.IsComplete = inputTodo.IsComplete;
-
-//     await db.SaveChangesAsync();
-
-//     return Results.NoContent();
-// });
-
-// app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
-// {
-//     if (await db.Todos.FindAsync(id) is Todo todo)
-//     {
-//         db.Todos.Remove(todo);
-//         await db.SaveChangesAsync();
-//         return Results.Ok(todo);
-//     }
-
-//     return Results.NotFound();
-// });
-
-// app.Run();
