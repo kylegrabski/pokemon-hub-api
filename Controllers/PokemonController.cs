df5b8eca54d2aba3c31c2a34c0dfc536274597ce
kylegrabski <kyle.grabski@gmail.com>
2023-05-02 22:12:21 -0700

    WIP add pagination

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -13,9 +13,19 @@ public class PokemonController : ControllerBase
     public PokemonController(PokemonService pokemonService) => 
         _pokemonService = pokemonService;
 
-    [HttpGet]
-    public async Task<List<Pokemon>> Get() =>
-        await _pokemonService.GetAsync();
+    // [HttpGet]
+    // public async Task<List<Pokemon>> Get() =>
+    //     await _pokemonService.GetAsync();
+    [HttpGet("{pageNumber}/{pageSize}")]
+    public async Task<ActionResult<IEnumerable<Pokemon>>> Get(int pageNumber, int pageSize)
+    {
+        Console.WriteLine($"{pageNumber} - {pageSize}");
+        var pokemon = await _pokemonService.GetAsync();
+
+        var pagedPokemon = pokemon.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+
+        return Ok(pagedPokemon);
+    }
 
     [HttpGet("{Name}")]
     public async Task<ActionResult<Pokemon>> Get(string name)
